3f411be36a69478139027870fe265b637c5751a2
Charlie Crease-Huggett <charlie.crease-huggett@Charlies-MacBook-Pro.local>
2024-07-25 11:46:19 +0100

    bla

--- src/ProdViewer.js
+++ src/ProdViewer.js
@@ -1,21 +1,20 @@
-// src/ProdViewer.js
-import React, { useRef, useState, useEffect } from "react";
-import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import React, { useRef, useState, useEffect, Suspense } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
 import { useTexture } from "@react-three/drei";
 import "./ProdViewer.css";
 
-const Image360 = ({ images, config, currentIndex }) => {
+const Image360 = ({ images, currentIndex }) => {
   const textures = useTexture(images); // load in images
 
   return (
     // Create a mesh with a plane geometry and a basic material
     <mesh>
-      // add plane and set size todo: improve calculation to work with other
-      image sizes
+      {/* Add plane and set size */}
       <planeGeometry
         args={[textures[0].image.width / 100, textures[0].image.height / 100]}
       />
-      <meshBasicMaterial map={textures[currentIndex]} /> // draw image to screen
+      <meshBasicMaterial map={textures[currentIndex]} />{" "}
+      {/* Draw image to screen */}
     </mesh>
   );
 };
@@ -23,10 +22,10 @@ const Image360 = ({ images, config, currentIndex }) => {
 const animationDuration = 4000; // time in milliseconds
 
 const ProdViewer = ({ images }) => {
-  // set initial state
+  // Set initial state
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  // config object to manage play/pause and speed
+  // Config object to manage play/pause and speed
   const [config, setConfig] = useState({
     play: true,
     totalImages: images.length,
@@ -37,27 +36,34 @@ const ProdViewer = ({ images }) => {
   const isDragging = useRef(false);
   const startX = useRef(0);
 
-  // constrain number of pixels when moving mouse
+  // Constrain number of pixels when moving mouse
   const pixelsPerImage = 300 / config.totalImages;
 
-  // handle mouse events
+  // Handle mouse events
   const handleMouseDown = (e) => {
     isDragging.current = true;
     startX.current = e.clientX;
-    config.isDragged = true;
+    setConfig((prevConfig) => ({
+      ...prevConfig,
+      isDragged: true,
+    }));
   };
 
   const handleMouseUp = () => {
     isDragging.current = false;
+    setConfig((prevConfig) => ({
+      ...prevConfig,
+      isDragged: false,
+    }));
   };
 
   const handleMouseMove = (e) => {
     if (isDragging.current) {
-      const dx = e.clientX - startX.current; // calculate change in x
-      const imageIndexChange = Math.floor(dx / pixelsPerImage); // ensure animation can be achieved with limited movement
+      const dx = e.clientX - startX.current; // Calculate change in x
+      const imageIndexChange = Math.floor(dx / pixelsPerImage); // Ensure animation can be achieved with limited movement
       const newIndex =
         (currentIndex + imageIndexChange + config.totalImages) %
-        config.totalImages; // constrain index to be within bounds
+        config.totalImages; // Constrain index to be within bounds
       setCurrentIndex(newIndex);
       startX.current = e.clientX; // Update the startX to current position
     }
@@ -68,7 +74,7 @@ const ProdViewer = ({ images }) => {
       ...prevConfig,
       play: !prevConfig.play,
     }));
-  }; // create a function to toggle play/pause
+  }; // Create a function to toggle play/pause
 
   useEffect(() => {
     if (config.play && !config.isDragged) {
@@ -77,7 +83,7 @@ const ProdViewer = ({ images }) => {
       }, config.speed);
       return () => clearInterval(interval);
     }
-  }, [config.play, config.speed, config.totalImages]); // performs animation based on speed
+  }, [config.play, config.speed, config.totalImages]); // Performs animation based on speed
 
   return (
     <div className="viewer-wrapper">
@@ -88,20 +94,17 @@ const ProdViewer = ({ images }) => {
         onMouseUp={handleMouseUp}
         onMouseLeave={handleMouseUp} // Ensure dragging stops if cursor leaves the viewer
       >
-        <Canvas>
-          <ambientLight intensity={0.5} />
-          <pointLight position={[10, 10, 10]} />
-          <Image360
-            images={images}
-            config={config}
-            currentIndex={currentIndex}
-          />
-        </Canvas>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Canvas>
+            <ambientLight intensity={0.5} />
+            <pointLight position={[10, 10, 10]} />
+            <Image360 images={images} currentIndex={currentIndex} />
+          </Canvas>
+        </Suspense>
       </div>
       <button className="pause-button" onClick={togglePlay}>
         {config.play ? "Pause" : "Play"}
-      </button>{" "}
-      // button to play/pause animation
+      </button>
     </div>
   );
 };
